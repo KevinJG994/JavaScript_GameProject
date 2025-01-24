accaea4cf34b725455937819806a016d3ac3f16e
KevinJG994 <KevinJG994@gmail.com>
2025-01-24 17:46:13 +0000

    Intro view done

---
+++ src/script.js
@@ -0,0 +1,19 @@
+let modal = document.getElementById("introModal");
+
+let btn = document.getElementById("modalButton");
+
+let span = document.getElementsByClassName("closeModal")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
