5292767875f8d91556cec405b0075d6fad518035
KevinJG994 <KevinJG994@gmail.com>
2025-01-29 14:45:08 +0000

    [Add] More difficulty

--- src/script.js
+++ src/script.js
@@ -28,27 +28,7 @@ window.onclick = function (event) {
   }
 };
 
-
-function soundTrack() {
-  const audioGame = new Audio('../Music/main_theme.mp3')
-  audioGame.volume = 0.1;
-  audioGame.play()
-}
-
-function gameTrack() {
-  const audioGame = new Audio('../Music/game_theme.mp3')
-  audioGame.volume = 0.05;
-  audioGame.play()
-}
-
-
 // Navegacion entre paneles
-if(gamePanel.style.display = "none") {
-  soundTrack()
-} else {
-  gameTrack()
-}
-
 startButton.onclick = () => {
   introPanel.style.display = "none";
   gamePanel.style.display = "flex";
@@ -88,63 +68,84 @@ function playGame() {
 
   //   Aparecen Diglett's random
   const gameInterval = setInterval(() => {
+    // Determinamos cuántos Diglett queremos que aparezcan en cada intervalo
+    let diglettCount;
 
-    const randomNumber = Math.floor(Math.random() * celda.length);
-    const randomDiglett = Math.floor(Math.random() * 3);
-    let diglett;
-
-    switch (randomDiglett) {
-      case 0:
-        diglett = new Diglett("../Images/Diglett.png", "150px");
-
-        diglett.onClick(() => {
-          const diglettAudio = new Audio('../Music/diglett_sound.mp3');
-          diglettAudio.play();
-
-          score += 10;
-          scoreLabel.innerText = `Puntuación: ${score}`;
-          finalScore.innerText = `Puntuación Final: ${score}`;
-          diglett.remove();
-        });
-        break;
-
-      case 1:
-        diglett = new Diglett("../Images/Diglett-Alola.png", "200px");
-
-        diglett.onClick(() => {
-          const alolanAudio = new Audio('../Music/alolan_sound.mp3');
-          alolanAudio.play();
-
-          counter -= 6;
-          time.innerText = `Tiempo: ${counter}`;
-          diglett.remove();
-        });
-        break;
-
-      case 2:
-        diglett = new Diglett("../Images/Wiglett.png", "150px");
-
-        diglett.onClick(() => {
-          const wiglettAudio = new Audio('../Music/wiglett_sound.mp3');
-          wiglettAudio.play();
-
-          counter += 6;
-          time.innerText = `Tiempo: ${counter}`;
-          diglett.remove();
-        });
-        break;
+    if (score >= 100) {
+      diglettCount = Math.floor(Math.random() * 3) + 1;
+    } else {
+      diglettCount = 1;
     }
 
-    diglett.spawn(celda[randomNumber]);
+    // Creamos un arreglo para llevar el control de las celdas ocupadas
+    const occupiedCells = [];
 
-    setTimeout(() => {
-      diglett.remove();
-    }, 1000);
+    for (let i = 0; i < diglettCount; i++) {
+      let randomNumber;
+
+      // Buscamos una casilla no ocupada
+      do {
+        randomNumber = Math.floor(Math.random() * celda.length);
+      } while (occupiedCells.includes(randomNumber));
+      // Marcamos la casilla como ocupada
+      occupiedCells.push(randomNumber);
+
+      const randomDiglett = Math.floor(Math.random() * 10);
+
+      let diglett;
+
+      switch (true) {
+        case (randomDiglett < 6):
+          diglett = new Diglett("../Images/Diglett.png", "150px");
+
+          diglett.onClick(() => {
+            const diglettAudio = new Audio('../Music/diglett_sound.mp3');
+            diglettAudio.play();
+
+            score += 10;
+            scoreLabel.innerText = `Puntuación: ${score}`;
+            finalScore.innerText = `Puntuación Final: ${score}`;
+            diglett.remove();
+          });
+          break;
+
+        case (randomDiglett < 8):
+          diglett = new Diglett("../Images/Diglett-Alola.png", "200px");
+
+          diglett.onClick(() => {
+            const alolanAudio = new Audio('../Music/alolan_sound.mp3');
+            alolanAudio.play();
+
+            counter -= 6;
+            time.innerText = `Tiempo: ${counter}`;
+            diglett.remove();
+          });
+          break;
+
+        case (randomDiglett < 10):
+          diglett = new Diglett("../Images/Wiglett.png", "150px");
+
+          diglett.onClick(() => {
+            const wiglettAudio = new Audio('../Music/wiglett_sound.mp3');
+            wiglettAudio.play();
+
+            counter += 6;
+            time.innerText = `Tiempo: ${counter}`;
+            diglett.remove();
+          });
+          break;
+      }
+
+      diglett.spawn(celda[randomNumber]);
+
+      setTimeout(() => {
+        diglett.remove();
+      }, 800);
+    }
   }, 2000);
-
+  
   if (counter <= 0) {
     clearInterval(gameInterval);
     return;
   }
 }
-
