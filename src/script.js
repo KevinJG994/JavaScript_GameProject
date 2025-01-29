eeb23cd608f89e317d760ed36421090fb4ca78ef
KevinJG994 <KevinJG994@gmail.com>
2025-01-29 12:28:32 +0000

    [Add] Efect sounds

--- src/script.js
+++ src/script.js
@@ -2,6 +2,18 @@ let modal = document.getElementById("introModal");
 let modalBtn = document.getElementById("modalButton");
 let span = document.getElementsByClassName("closeModal")[0];
 
+const introPanel = document.getElementsByClassName("introGame-panel")[0];
+const gamePanel = document.getElementsByClassName("game-panel")[0];
+const gameOverPanel = document.getElementsByClassName("gameOver-panel")[0];
+
+const startButton = document.getElementById("startButton");
+const resetButton = document.getElementById("resetButton");
+let time = document.getElementById("time");
+
+let scoreLabel = document.getElementById("score");
+let finalScore = document.getElementById("finalScore");
+let celda = document.getElementsByClassName("row");
+
 modalBtn.onclick = function () {
   modal.style.display = "block";
 };
@@ -16,14 +28,26 @@ window.onclick = function (event) {
   }
 };
 
-// Navegacion entre paneles
-const introPanel = document.getElementsByClassName("introGame-panel")[0];
-const gamePanel = document.getElementsByClassName("game-panel")[0];
-const gameOverPanel = document.getElementsByClassName("gameOver-panel")[0];
 
-const startButton = document.getElementById("startButton");
-const resetButton = document.getElementById("resetButton");
-let time = document.getElementById("time");
+function soundTrack() {
+  const audioGame = new Audio('../Music/main_theme.mp3')
+  audioGame.volume = 0.1;
+  audioGame.play()
+}
+
+function gameTrack() {
+  const audioGame = new Audio('../Music/game_theme.mp3')
+  audioGame.volume = 0.05;
+  audioGame.play()
+}
+
+
+// Navegacion entre paneles
+if(gamePanel.style.display = "none") {
+  soundTrack()
+} else {
+  gameTrack()
+}
 
 startButton.onclick = () => {
   introPanel.style.display = "none";
@@ -35,11 +59,8 @@ resetButton.onclick = () => {
   window.location.reload();
 };
 
-// Iniciar Juego
-let scoreLabel = document.getElementById("score");
-let finalScore = document.getElementById("finalScore");
-let celda = document.getElementsByClassName("row");
 
+// Iniciar Juego
 function playGame() {
   let counter = 30;
   let score = 0;
@@ -61,19 +82,12 @@ function playGame() {
       time.style.color = "white";
     }
 
-    if (counter <= 0) {
-      time.innerText = `Tiempo: 0`;
-    }
-
     counter--;
   }, 1000);
 
+
   //   Aparecen Diglett's random
   const gameInterval = setInterval(() => {
-    if (counter <= 0) {
-      clearInterval(gameInterval);
-      return;
-    }
 
     const randomNumber = Math.floor(Math.random() * celda.length);
     const randomDiglett = Math.floor(Math.random() * 3);
@@ -84,7 +98,10 @@ function playGame() {
         diglett = new Diglett("../Images/Diglett.png", "150px");
 
         diglett.onClick(() => {
-          score++;
+          const diglettAudio = new Audio('../Music/diglett_sound.mp3');
+          diglettAudio.play();
+
+          score += 10;
           scoreLabel.innerText = `Puntuación: ${score}`;
           finalScore.innerText = `Puntuación Final: ${score}`;
           diglett.remove();
@@ -95,6 +112,9 @@ function playGame() {
         diglett = new Diglett("../Images/Diglett-Alola.png", "200px");
 
         diglett.onClick(() => {
+          const alolanAudio = new Audio('../Music/alolan_sound.mp3');
+          alolanAudio.play();
+
           counter -= 6;
           time.innerText = `Tiempo: ${counter}`;
           diglett.remove();
@@ -105,6 +125,9 @@ function playGame() {
         diglett = new Diglett("../Images/Wiglett.png", "150px");
 
         diglett.onClick(() => {
+          const wiglettAudio = new Audio('../Music/wiglett_sound.mp3');
+          wiglettAudio.play();
+
           counter += 6;
           time.innerText = `Tiempo: ${counter}`;
           diglett.remove();
@@ -118,5 +141,10 @@ function playGame() {
       diglett.remove();
     }, 1000);
   }, 2000);
+
+  if (counter <= 0) {
+    clearInterval(gameInterval);
+    return;
+  }
 }
 
